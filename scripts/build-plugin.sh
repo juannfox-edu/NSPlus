a9df497b91fa71b089bbdff7a47a6bab9e5cdd28
Juan Fox <juannfox@hotmail.com>
2024-04-24 19:49:07 -0300

    Add script for building plugin

---
+++ scripts/build-plugin.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+target="$HOME/Downloads/local_nsplus.zip"
+temp_folder="nsplus"
+
+mkdir $temp_folder
+cp -r ./* $temp_folder
+rm -rf $target
+
+if zip "${target}" -r $temp_folder;then
+    echo "Build success: $target"
+else
+    echo "Build failed with RC $?."
+fi
+
+rm -rf $temp_folder

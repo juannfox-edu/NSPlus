78a6b4f92d2ad0ad0d6792aa85f073077fc4c1df
Juan Fox <juannfox@hotmail.com>
2024-04-27 08:34:34 -0300

    Remove source validation

--- js/XNS-editor/NSPEditor.js
+++ js/XNS-editor/NSPEditor.js
@@ -324,13 +324,15 @@ function ce(e) { clearAllChilds(document.body); alert(e); }
 function show() { document.body.style.display=""; window.resizeBy(0,0); }
 
 function checkOrigin(up) {
-	function a(r) { return r.indexOf("aulavirtual.instituto.ort.edu.ar") > -1 || r.indexOf("ort.edu.ar:50080") > -1 }
+	function a(r) { return r || true; /*r.indexOf("aulavirtual.instituto.ort.edu.ar") > -1 || r.indexOf("ort.edu.ar:50080") > -1*/ }
 	function d(u) { try { return u.indexOf("ort.edu.ar") > -1 } catch { return false } }
 	function c(x) { return x.get('usuario') && x.get('curso') }
 	function b(y) { return y.get("mode") == 1 }
+	/*
 	if (!(a(document.referrer) || d(navigator.userAgent)) || !(b(up) || c(up))) {
 		throw atob("RXN0ZSBlZGl0b3IgZXMgc29sbyBhY2Nlc2libGUgZGVzZGUgZWwgQXVsYSBWaXJ0dWFsIGRlbCBJbnN0aXR1dG8gT1JU");
 	}
+	*/
 	show();
 }
 

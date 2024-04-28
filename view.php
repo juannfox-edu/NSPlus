012185523b4da89cb59b62eb2d0e152d0879e6a1
Juan Fox <juannfox@hotmail.com>
2024-04-28 19:02:34 -0300

    Move JS and CSS imports to PHP

--- view.php
+++ view.php
@@ -24,20 +24,38 @@
 
 require_once('../../config.php');
 
-$PAGE->set_url(new moodle_url('/local/nsplus/view.php'));
+$PAGE->set_url('/local/nsplus/view.php');
+$PAGE->set_title('NSPlus');
 $PAGE->set_context(context_system::instance());
-$PAGE->set_title(get_string('pluginname', 'local_nsplus'));
 
-$PAGE->requires->js(new moodle_url('/local/nsplus/editor/js/html2canvas.js'));
-$PAGE->requires->js(new moodle_url('/local/nsplus/editor/js/Enumeration.js'));
-$PAGE->requires->js(new moodle_url('/local/nsplus/editor/js/ClassConstructor.js'));
-$PAGE->requires->js(new moodle_url('/local/nsplus/editor/js/BaseDiagram.js'));
-$PAGE->requires->js(new moodle_url('/local/nsplus/editor/js/DiagramObject.js'));
-$PAGE->requires->js(new moodle_url('/local/nsplus/editor/js/XNSDiagram.js'));
-$PAGE->requires->js(new moodle_url('/local/nsplus/NSPlus.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-core/html2canvas.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-core/Enumeration.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-core/ClassConstructor.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-core/BaseDiagram.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-core/DiagramObject.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-core/XNSDiagramMaker.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-editor/templates.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-editor/NSPUtils.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-editor/utils.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-editor/NSPPDF.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-editor/NSPFile.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-editor/DiagramContainer.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-editor/DiagramsMenu.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-editor/buttonsHandlers.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-editor/buttons.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-editor/StatementsMenu.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-editor/NSPDiagram.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-editor/NSPProject.js'));
+$PAGE->requires->js(new moodle_url('js/XNS-editor/NSPEditor.js'));
 
-$PAGE->requires->css(new moodle_url('/local/nsplus/editor/css/XNSDEditor.css'));
-$PAGE->requires->css(new moodle_url('/local/nsplus/editor/css/XNSDiagram.css'));
+$PAGE->requires->css(new moodle_url('css/w3.css'));
+$PAGE->requires->css(new moodle_url('css/NSPDiagram.css'));
+$PAGE->requires->css(new moodle_url('css/NSPEditor.css'));
+$PAGE->requires->css(new moodle_url('css/NSPMenu.css'));
+$PAGE->requires->css(new moodle_url('css/NSPPDF.css'));
+$PAGE->requires->css(new moodle_url('css/animations.css'));
+$PAGE->requires->css(new moodle_url('css/switch-button.css'));
+$PAGE->requires->css(new moodle_url('css/NSPColors.css'));
 
 echo $OUTPUT->header();
 include('index.html');
